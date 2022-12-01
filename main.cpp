d6319f3cbe2c61f2d9fcd562eebd1a0dd131a9c3
jvanvelzen <jve@joz.nl>
2022-12-01 10:02:45 +0100

    ADDED gitignore

--- main.cpp
+++ main.cpp
@@ -1,18 +1,6 @@
 #include <iostream>
 
 int main(int argc,char* argv[]) {
-    std::cout << "Hello! \n";
-    if(argc == 1){
-        std::cout << "Please enter arguments";
-        return 0;
-    }
-    if(argc >= 2){
-        std::cout << "You passed " << argc-1<< " argument(s), here they are!:\n" ;
-
-        for( int i= 1; i < argc; i++ ) {
-            std::cout <<"\nargv[" << i << "]"<<": " <<argv[i]<<"\n";
-        }
-
-    }
+    std::cout << "Hello World!\n";
     return 0;
 }
